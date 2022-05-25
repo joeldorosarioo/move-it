94cfeb318c9d5076d5c1c5e200914a10f2b30866
Joel Oliveira <joeldorosarioo@gmail.com>
2022-05-25 12:26:45 -0300

    feat: creating the completed challenges component

---
+++ src/components/CompletedChallenges.tsx
@@ -0,0 +1,15 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContexts';
+
+import styles from '../stylesheets/components/CompletedChallenges.module.css';
+
+export function CompletedChallenges () {
+	const { challengesCompleted } = useContext(ChallengesContext);
+
+	return (
+		<div className={ styles.completedChallengesContainer }>
+			<span>Desafios completos</span>
+			<span>{ challengesCompleted }</span>
+		</div>
+	);
+}
