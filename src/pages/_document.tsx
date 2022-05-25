c4048165d816861d0a5afa2d0f87788e0f04a378
Joel Oliveira <joeldorosarioo@gmail.com>
2022-05-25 11:40:45 -0300

    feat: add initial setup

---
+++ src/pages/_document.tsx
@@ -0,0 +1,23 @@
+import { Head, Html, Main, NextScript } from "next/document";
+
+export default function Document () {
+	return (
+		<Html lang="en">
+			<Head>
+				<title>Home - Move.it</title>
+
+				<meta name="description" content="Move.it is a project that combines the pomodoro technique with some challenges for the well-being of those who spend many hours using the computer." />
+
+				<link rel="shortcut icon" href="favicon.png" type="image/png" />
+
+				<link rel="preconnect" href="https://fonts.gstatic.com" />
+				<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" />
+			</Head>
+
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	)
+}
